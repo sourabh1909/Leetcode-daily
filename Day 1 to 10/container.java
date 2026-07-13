d9df66b6e68fea44ab307a86cd6bbbc9bd7b376f
Sourabh1909 <sourabhphalatane19@gmail.com>
2026-07-13 19:12:45 +0530

    first day

--- Day 1 to 10/container.java
+++ Day 1 to 10/container.java
@@ -1,3 +1,25 @@
+
 public class container {
-    
+     public int maxArea(int[] height) {
+        int area = 0;
+        int lp = 0;
+        int rp = height.length-1;
+
+        int maxWater = 0;
+
+        while(lp < rp){
+            int width = rp - lp;
+            int hei = Math.min(height[lp],height[rp]);
+
+            area = width*hei;
+            maxWater = Math.max(maxWater,area);
+            if(height[lp] < height[rp]){
+                lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+        return maxWater;
+    }
 }
