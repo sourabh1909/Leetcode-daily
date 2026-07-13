cdc4d484b0ba977f63e6c4333f325b8782af2972
Sourabh1909 <sourabhphalatane19@gmail.com>
2026-07-13 19:45:48 +0530

    Day One Complete

--- Day 1 to 10/trappingWater.java
+++ Day 1 to 10/trappingWater.java
@@ -1,3 +1,27 @@
 public class trappingWater {
-    
+    public int trap(int[] height) {
+        int leftMax[] = new int[height.length];
+        int rightMax[] = new int[height.length];
+        int n = height.length;
+
+        int maxWater = 0;
+        leftMax[0] = height[0]; 
+        for(int i=1;i<n;i++){
+            leftMax[i] = Math.max(height[i],leftMax[i-1]);
+        }
+
+        rightMax[n-1] = height[n-1];
+        for(int i = n-2;i>=0;i--){
+            rightMax[i] = Math.max(rightMax[i+1],height[i]);
+        }
+        int waterLevel = 0;
+
+        for(int i = 0;i<n;i++){
+            waterLevel = Math.min(leftMax[i],rightMax[i]);
+
+            maxWater += waterLevel-height[i];
+        }
+
+        return maxWater;
+    }
 }
